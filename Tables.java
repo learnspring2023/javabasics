f501e0501edc7b03f8992d54e1bb9fcb1d799442
learnspring2023 <137409193+learnspring2023@users.noreply.github.com>
2024-01-06 13:16:51 +0530

    Create Tables.java

---
+++ Tables.java
@@ -0,0 +1,13 @@
+package Trend;
+
+public class Tables {
+	public static void main(String[] args) {
+
+		int maggi =19;
+		//int; true cond; iteration
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(maggi*i);
+		}
+	}
+
+}
